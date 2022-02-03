1b6e668bf77176af006b4dae53f5f342f8a0f260
Natt Forclaz <natt.for.m@gmail.com>
2022-02-02 23:22:28 -0300

    Practica Ejercicio 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,16 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var yourName = prompt("Please enter your name", "Mr. X");
+	
+	alert("Hello " + yourName + "! How are you today?");
 }
 
+/*function myFunction() {
+	let person = prompt("Please enter your name", "Mr. X");
+	if (person != null) {
+	  document.getElementById("demo").innerHTML =
+	  "Hello " + person + "! How are you today?";
+	}
+  }
+  */
