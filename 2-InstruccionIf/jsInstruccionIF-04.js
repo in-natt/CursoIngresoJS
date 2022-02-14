de6c8a2739be031a67ce2f4381748ae682b39475
Natt Forclaz <natt.for.m@hotmail.com>
2022-02-13 23:47:27 -0300

    Practica IF 4 y 5

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,17 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
+// Al ingresar una edad debemos informar si la persona es adolescente,
+// edad entre 13 y 17 años (inclusive)
+function mostrar() {
+	var edad;
+	var parseoEdad;
 
+	edad = txtIdEdad.value;
+	parseoEdad = parseInt(edad);
 
-}//FIN DE LA FUNCIÓN
+	if (parseoEdad > 12 && parseoEdad < 18) {
+		alert("Eres Adolescente :)");
+		// console.log(parseoEdad);
+	}
+	else {
+		alert("Su edad es: " + parseoEdad);
+	}
+}
--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,15 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
+//Al ingresar una edad solo debemos informar si la persona NO es adolescente.
+function mostrar() {
+	var edad;
+	var parseoEdad;
 
-}//FIN DE LA FUNCIÓN
+	edad = txtIdEdad.value;
+	parseoEdad = parseInt(edad);
+
+	if (parseoEdad < 13 || parseoEdad > 17) {
+		alert("No eres Adolescente D:");
+	}
+	else {
+		alert("Su edad es: " + parseoEdad);
+	}
+}
