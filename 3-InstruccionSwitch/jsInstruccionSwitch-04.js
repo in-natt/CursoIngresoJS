f4c6d71e7583d195d8db732afaf969faeb1c1705
Natt Forclaz <natt.for.m@hotmail.com>
2022-02-23 17:59:42 -0300

    Practica ejercicios SWITCH 1 a 5

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,24 @@
-function mostrar()
-{
+// al seleccionar un mes informar.
+// si tiene 28 días.
+// si tiene 30 días.
+// si tiene 31 días.
+
+function mostrar() {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	var mesDelAnio = txtIdMes.value;
 
-
-
-}//FIN DE LA FUNCIÓN
+	switch (mesDelAnio) {
+		case "Febrero":
+			alert("Tiene 28 días");
+			break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("Tiene 30 días");
+			break;
+		default:
+			alert("Tiene 31 días");
+			break;
+	}
+}
--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,11 +1,23 @@
-function mostrar()
-{
+// Al ingresar una hora, informar:
+// si está entre las 7 y las 11 : "Es de mañana.".
+function mostrar() {
 	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
-	
-	
+	var horaDelDia;
+	var horaParseada;
 
+	horaDelDia = document.getElementById("txtIdHora").value;
+	horaParseada = parseInt(horaDelDia);
 
-
-}//FIN DE LA FUNCIÓN
+	switch (horaParseada) {
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			alert("Es de mañana");
+			break;
+		default:
+			alert("Gracias");
+			break;
+	}
+}
