c9d96ef33478b65628f0f99941f36b0510cab389
Natt Forclaz <natt.for.m@hotmail.com>
2022-03-04 14:20:41 -0300

    TP 1.FerreteFacturacion

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,15 +3,90 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
+function Sumar() {
+
+    //Declaracion de variables
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var unoParseado;
+    var dosParseado;
+    var tresParseado;
+    var texto;
+    var resultado;
+
+    //Asignacion
+    precioUno = txtIdPrecioUno.value;
+    precioDos = txtIdPrecioDos.value;
+    precioTres = txtIdPrecioTres.value;
+    texto = "La SUMA total de los productos es: $";
+
+    //Parseo
+    unoParseado = parseFloat(precioUno);
+    dosParseado = parseFloat(precioDos);
+    tresParseado = parseFloat(precioTres);
+
+
+    //Operacion
+    resultado = (unoParseado + dosParseado + tresParseado).toFixed(2);
+    alert(texto + resultado);
+
 }
-function Promedio () 
-{
-	
+function Promedio() {
+
+    //Declaracion de variables
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var unoParseado;
+    var dosParseado;
+    var tresParseado;
+    var texto;
+    var resultado;
+
+    //Asignacion
+    precioUno = txtIdPrecioUno.value;
+    precioDos = txtIdPrecioDos.value;
+    precioTres = txtIdPrecioTres.value;
+    texto = "El PROMEDIO de los importes es: $";
+
+    //Parseo
+    unoParseado = parseFloat(precioUno);
+    dosParseado = parseFloat(precioDos);
+    tresParseado = parseFloat(precioTres);
+
+    //Operacion
+    resultado = (unoParseado + dosParseado + tresParseado) / 3;
+    alert(texto + resultado.toFixed(2));
+
 }
-function PrecioFinal () 
-{
-	
+function PrecioFinal() {
+
+    //Declaracion de variables
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var unoParseado;
+    var dosParseado;
+    var tresParseado;
+    var texto;
+    var resultado;
+    var suma;
+
+    //Asignacion
+    precioUno = txtIdPrecioUno.value;
+    precioDos = txtIdPrecioDos.value;
+    precioTres = txtIdPrecioTres.value;
+
+    //Parseo
+    unoParseado = parseFloat(precioUno);
+    dosParseado = parseFloat(precioDos);
+    tresParseado = parseFloat(precioTres);
+
+    //Operacion
+    suma = unoParseado + dosParseado + tresParseado;
+    resultado = suma * 1.21;
+    texto = "El Precio Final " + suma + " + IVA es de: $";
+    alert(texto + resultado.toFixed(2));
+
 }
