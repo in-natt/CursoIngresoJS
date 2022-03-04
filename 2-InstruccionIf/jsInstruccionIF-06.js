34c5dbd796661e09d790ae3e0bc6bb78bd735dcd
Natt Forclaz <natt.for.m@hotmail.com>
2022-03-04 14:11:29 -0300

    Practica IF 6, 7 y 8

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,25 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
+// Al ingresar una edad debemos informar si la persona es:
+// - mayor de edad (mas de 18 años)
+// - adolescente (entre 13 y 17 años)
+// - niño (menor a 13 años)
+function mostrar() {
+	var edad;
+	var parseoEdad;
 
+	edad = document.getElementById("txtIdEdad").value;
+	parseoEdad = parseInt(edad);
 
-
-}//FIN DE LA FUNCIÓN
+	if (parseoEdad > 17) 
+	{
+		alert("Sos MAYOR! Tu edad es " + parseoEdad);
+	}
+	else {
+		if (parseoEdad > 12 && parseoEdad < 18) 
+		{
+			alert("Sos Adolescente. Tu edad es " + parseoEdad)
+		}
+		else {
+			alert("Sos un Niño. Tu edad es " + parseoEdad);
+		}
+	}
+}
--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,17 @@
-function mostrar()
-{
-	//tomo la edad  
-	alert("ok");
-	
+/*Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero",
+mostrar el siguiente mensaje:'Es muy pequeño para NO ser soltero.'*/
+function mostrar() {
+	var edad;
+	var estadoCivil;
 
+	edad = document.getElementById("txtIdEdad").value;
+	edad = parseInt(edad);
+	estadoCivil = document.getElementById("estadoCivil").value;
 
-}//FIN DE LA FUNCIÓN
+	if (edad < 18 && estadoCivil != "Soltero") {
+		alert('Es muy pequeño para NO ser soltero');
+	}
+	else {
+		alert('Su edad es: ' + edad + '\nSu estado civil es: ' + estadoCivil)
+	}
+}
