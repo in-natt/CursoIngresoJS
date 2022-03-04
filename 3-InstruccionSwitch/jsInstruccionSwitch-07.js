e40b7edc3f47c46d3353e4a2c62e018ad5e9a64c
Natt Forclaz <natt.for.m@hotmail.com>
2022-03-04 14:12:50 -0300

    Practica ejercicios SWITCH 6 a 8

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,25 @@
+/*Al selecionar un destino, indicar el punto cardinal de nuestro pais
+en donde se encuentra Norte, Sur, Este u Oeste
+"Bariloche" - Oeste
+"Cataratas" - Norte
+"Mar del plata" - Este
+"Ushuaia" - Sur */
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
-
-}//FIN DE LA FUNCIÓN
+	var destinoIngresado = document.getElementById("txtIdDestino").value;
+	
+	switch (destinoIngresado) {
+		case "Bariloche":
+			alert("Su destino " + destinoIngresado + " esta al OESTE");			
+			break;
+		case "Cataratas":
+			alert("Su destino " + destinoIngresado + " esta al NORTE");			
+			break;
+		case "Mar del plata":
+			alert("Su destino " + destinoIngresado + " esta al ESTE");			
+			break;
+		case "Ushuaia":
+			alert("Su destino " + destinoIngresado + " esta al SUR");			
+			break;
+	}
+}
--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,20 @@
+//Al seleccionar un destino informar si hace FRIO o CALOR en ese destino
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
-
-}//FIN DE LA FUNCIÓN
+	var destinoIngresado = document.getElementById("txtIdDestino").value;
+	
+	switch (destinoIngresado) {
+		case "Bariloche":
+			alert("En " + destinoIngresado + " hace FRIO");			
+			break;
+		case "Cataratas":
+			alert("En " + destinoIngresado + " hace CALOR");			
+			break;
+		case "Mar del plata":
+			alert("En " + destinoIngresado + " hace CALOR");			
+			break;
+		case "Ushuaia":
+			alert("En " + destinoIngresado + " hace FRIO");			
+			break;
+	}
+}
