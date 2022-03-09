f9783ae2ccd54ae889e17d60d184879834de2ebb
Natt Forclaz <natt.for.m@hotmail.com>
2022-03-09 14:09:16 -0300

    Files added for Recuperatorio

--- 1-EntradaSalida/TEST2.js
+++ 1-EntradaSalida/TEST2.js
@@ -1,12 +1,4 @@
 /*
-2-Nos ingresan una cantidad indeterminada de alumnos inscriptos en programación 1,
-validando los datos ingresados:
-División [A,B, C] ,nombre del alumno ,legajo [del 1 al 10000] , si es recursante o no y nota.
-informar:
-a)La división con más aspirantes.
-b)el nombre recursante con mayor nota
-c)el nombre del que no es recursante y tiene  la menor nota.
-Pedir datos por prompt y mostrar por document.write o console.log
 */
 
 function mostrar() {
--- 1-EntradaSalida/TEST3.js
+++ 1-EntradaSalida/TEST3.js
@@ -1,13 +1,6 @@
 /*
-3- De 5  personas que suben a un avión necesitamos tomar y validar los siguientes datos:
-nombre , asiento [1 al 100],importe del pasaje de 0 a 300000, sexo [m,f o nb] y edad [1 a 100].
-a)Informar el último asiento ingresado con el nombre  y su importe.
-b)La edad promedio de las mujeres.
-c)el maximo importe ingresado por un pasaje de un pasajero sexo nb.
-d)el minimo importe ingresado por un pasaje de un pasajero sexo f.
-Pedir datos por prompt y mostrar por document.write o console.log
 */
 
 function mostrar() {
-
+	
 }
